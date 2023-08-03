d345d6a6142ad941d4a25bbe0f48bc1bd28e958a
artoniado <artoniado1337@gmail.com>
2023-08-02 22:12:15 -0400

    added os_env

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-
+import os
 '''
 Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
@@ -25,7 +25,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY_A']
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
